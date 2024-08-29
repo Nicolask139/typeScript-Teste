b32606d6be89c412611e4e305bcd8c3834c29d60
Nicolask139 <Nicolaskiy@gmail.com>
2024-08-28 21:35:14 -0300

    testesPersonagens

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import prompt from "prompt-sync";
+import {Personagem} from './personagem'
+
+let person: Personagem = new Personagem("Goku", 10, 20, 30, 2);
+let teclado = prompt();
+let option: number = 0;
+
+console.log ("Inicio jogo", person)
+
+while(option != 9){
+    console.log("=====Personagem======");
+    console.log("1. Treinar ataque");
+    console.log("2. Treinar defesa");
+    console.log("3. Descansar");
+    console.log("4. Entrar em Batalha"); 
+    console.log("9. sair");
+
+    option = +teclado("Escolha uma opção:")
+
+    switch (option) {
+        case 1:
+            person.treinarAtaque();
+            console.log(person.status())
+            break;
+
+        case 2:
+            person.treinarDefesa();
+            console.log(person.status())
+            break;
+                
+        case 3:
+            let horas: number = +teclado("Digite o número de horas: ");
+            person.descansar(horas);
+            console.log(person.status())
+            break;
+
+        case 4:
+            let rest: number = person.batalhar();
+            console.log(`Enta batalha custou: ${rest.toFixed()} de energia`);
+            console.log(person.status())
+            break;
+            
+        default:
+            break;
+    }
+}
--- src/personagem.ts
+++ src/personagem.ts
@@ -1,91 +1,60 @@
-import prompt from "prompt-sync"; 
-
-
-
-//forma1
-class Personagem {
-    nome: string = '';
-    energia: number = 0;
-    vida: number = 0;
-    ataque: number = 0;
-    defesa: number = 0;
-}
-
-let person = new Personagem();
-person.nome = "Goku";
-person.energia = 10;
-person.vida = 20;
-person.ataque = 30;
-person.defesa = 40;
-
-let teclado = prompt();
-let option: number = 0;
-
-while(option != 9){
-    console.log("=====Personagem======");
-    console.log("1. Treinar ataque");
-    console.log("2. Treinar defesa");
-    console.log("3. imprimir os dados");
-    console.log("9. sair");
-
-    option = +teclado("Escolha uma opção:")
-
-    switch (option) {
-        case 1:
-            person.ataque += 20;
-            break;
-
-        case 2:
-            person.defesa -= 10;
-            break;
-                
-        case 3:
-            console.log ("Personagem atualizado", person);
-            break;
-    
-        default:
-            break;
-    }
-}
-
-
-
-console.log("");
-
-//forma2 e 3
-
-/*class Personagem {
-    nome: string = 'vegetta';
-    energia: number = 0;
-    vida: number = 0;
-    ataque: number = 0;
-    defesa: number = 0;
-
-    constructor(nome: string){
-        this.nome = "Goku";
-        this.energia = 10;
-        this.vida = 20;
-        this.ataque = 30;
-        this.defesa = 40;
-    }
-}
-
-let person = new Personagem("kuririn");
-console.log("Personagem", person);*/
-
-//forma4
-
-/*class Personagem {
-    constructor(){
+export class Personagem {
+    constructor(
         public nome: string,
         public energia: number,
         public ataque: number,
-        public vida: number,
-        public defesa: number;
-    }
-}
+        public defesa: number,
+        public vida: number
+    ){}      
 
-person = new Personagem ("Goku",10,20,30,40);
-console.log("Personagem", person);*/
+    status(): string {
+        return (
+        "Guerreiro \n" +
+        "\nNome: " +
+        ("\nEnergia" + this.energia.toFixed(1)) +
+        ("\nAtaque" + this.ataque.toFixed(1)) +
+        ("\nDefesa" + this.defesa.toFixed(1))
+        );
+    }
+
+    treinarAtaque(): void{
+        this.ataque += Math.random() * 7;
+        this.energia -= Math.random() * 10;
+
+        if(this.defesa > 100){
+            this.defesa = 0;
+        }
+    }
+
+    treinarDefesa(): void{
+        this.defesa += Math.random() * 5;
+        this.energia -= Math.random() * 10;
+
+        if (this.defesa > 100){
+            this.energia = 100;
+        }
+    }
+
+    descansar(horas: number){
+        this.energia += horas * (Math.random() * 10);
+        if(this.energia > 100){
+            this.energia = 100;
+        }
+    }
+
+    batalhar(): number{
+        let percaBatalha: number = Math.random() * 100;
+        this.energia -= percaBatalha;
+        this.isDead();
+        return percaBatalha;
+    }
+
+    isDead(){
+        if(this.energia <= 0){
+            console.log ("Você morreu....");
+        }
+    }
+
+}
 
 
